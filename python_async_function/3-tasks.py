2842b49e0310ef3a53fb86fc4eb060c0044f8970
Joshuaburle <11491@holbertonschool.com>
2026-04-11 08:48:36 +0200

    Add folder python_async_function

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that defines a task creator for wait_random"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Returns an asyncio.Task for wait_random with given max_delay."""
+    return asyncio.create_task(wait_random(max_delay))
