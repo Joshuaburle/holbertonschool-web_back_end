679f08a5f3ead0946634c91fc826f75b0ae13282
Joshuaburle <11491@holbertonschool.com>
2026-04-22 21:31:40 +0200

    Add a bigger key for finding the dataset

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -37,27 +37,28 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a deletion-resilient page of data"""
+        data = self.indexed_dataset()
+
         if index is None:
             index = 0
 
-        indexed_data = self.indexed_dataset()
-
         # check valid inputs
-        assert isinstance(index, int) and index >= 0 and index < len(indexed_data)
+        assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
+        assert index < max(data.keys()) + 1
 
-        data = []
-        current_index = index
+        page_data = []
+        next_index = index
 
-        # collect items while skipping deleted indexes
-        while len(data) < page_size and current_index < len(indexed_data):
-            if current_index in indexed_data:
-                data.append(indexed_data[current_index])
-            current_index += 1
+        # collect page_size items, skipping missing indexes
+        while len(page_data) < page_size and next_index <= max(data.keys()):
+            if next_index in data:
+                page_data.append(data[next_index])
+            next_index += 1
 
         return {
-            "index": index,              # current start index
-            "next_index": current_index, # next index to query
-            "page_size": len(data),      # actual page size
-            "data": data                 # page data
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(page_data),
+            "data": page_data
         }
